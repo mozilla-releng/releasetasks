99ea1c0d2fdcd2886254316efa00d0d1b7a3ee48
Ben Hearsum <bhearsum@mozilla.com>
2015-07-27 15:31:08 -0400

    Add support for templates in multiple files.

--- releasetasks/__init__.py
+++ releasetasks/__init__.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 
+from os import path
+import yaml
 
-from jinja2 import Template, StrictUndefined
+from arrow import Arrow
+from jinja2 import Environment, FileSystemLoader, StrictUndefined
 
 from .tcutils import stable_slug_id
 
-def make_task_graph(template_stream, **template_kwargs):
-    #env = Environment(loader=FileSystemLoader(template_location), undefined=StrictUndefined)
-    template = Template(template_stream.read())
+
+DEFAULT_TEMPLATE_DIR = path.join(path.dirname(__file__), "templates")
+
+def make_task_graph(root_template="release_graph.yml.tmpl", template_dir=DEFAULT_TEMPLATE_DIR, **template_kwargs):
+    env = Environment(loader=FileSystemLoader(template_location), undefined=StrictUndefined)
+
+    template = env.get_template(root_template)
     template_vars = {
         "stable_slug_id": stable_slug_id,
-        "now": something,
+        "now": Arrow.now(),
     }
     template_vars.update(template_kwargs)
 
