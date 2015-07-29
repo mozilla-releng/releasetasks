79add208bf8c9a5545998c2476be46d818620627
Ben Hearsum <bhearsum@mozilla.com>
2015-07-29 14:28:34 -0400

    Add basic structure of a test.

--- releasetasks/__init__.py
+++ releasetasks/__init__.py
@@ -12,8 +12,9 @@ from .tcutils import stable_slug_id
 DEFAULT_TEMPLATE_DIR = path.join(path.dirname(__file__), "templates")
 
 
+
 def make_task_graph(root_template="release_graph.yml.tmpl", template_dir=DEFAULT_TEMPLATE_DIR, **template_kwargs):
-    # TODO: some validation of template_kwargs
+    # TODO: some validation of template_kwargs + defaults
     env = Environment(loader=FileSystemLoader(template_dir), undefined=StrictUndefined)
 
     now = arrow.now()
--- releasetasks/test/test_releasetasks.py
+++ releasetasks/test/test_releasetasks.py
@@ -5,10 +5,28 @@ import unittest
 from releasetasks import make_task_graph
 
 class TestMakeTaskGraph(unittest.TestCase):
+    maxDiff = 30000
+
     def testSimpleGraph(self):
+        # TODO: how to do this without manually rewriting insane amounts of template code?
+        # maybe manually rewriting it is the right thing to do?
         expected = {
         }
         generated = make_task_graph(
-            
+            updates_enabled=True,
+            l10n_platforms=None,
+            enUS_platforms=["win32", "macosx64"],
+            partial_updates={
+                "38.0": {
+                    "buildNumber": 1,
+                },
+                "37.0": {
+                    "buildNumber": 2,
+                },
+            },
+            branch="mozilla-beta",
+            product="firefox",
+            revision="abcdef123456",
+            balrog_api_root="https://fake.balrog/api",
         )
         self.assertEquals(expected, generated)
