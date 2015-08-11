f27b7aa8cef5d132ff421836d2a96b8f3ed6fefa
Ben Hearsum <bhearsum@mozilla.com>
2015-08-11 15:37:09 -0400

    Only add necessary scopes when necessary; sketch out some better ideas for unit tests.

--- releasetasks/test/test_releasetasks.py
+++ releasetasks/test/test_releasetasks.py
@@ -5,14 +5,31 @@ import unittest
 from releasetasks import make_task_graph
 
 class TestMakeTaskGraph(unittest.TestCase):
+    """Because of huge the graph gets, verifying every character of it is
+    impossible to maintain. Instead, we verify aspects of it. Eg, making sure
+    the correct number of funsize partials are happening, rather than verifying
+    the entire funsize tasks."""
     maxDiff = 30000
 
-    def testSimpleGraph(self):
-        # TODO: how to do this without manually rewriting insane amounts of template code?
-        # maybe manually rewriting it is the right thing to do?
-        expected = {
-        }
-        generated = make_task_graph(
+    def _do_common_assertions(self, graph):
+        if graph["tasks"]:
+            for t in graph["tasks"]:
+                task = t["task"]
+                self.assertEquals(task["priority"], "high")
+
+    def test_updates_disabled(self):
+        graph = make_task_graph(
+            updates_enabled=False
+        )
+
+        self._do_common_assertions(graph)
+        self.assertEquals(graph["tasks"], None)
+        self.assertEquals(graph["scopes"], None)
+
+    def test_minimal_graph(self):
+        """Tests a very minimal graph, with some platforms, l10n and other
+        unnecessary tasks disabled."""
+        graph = make_task_graph(
             updates_enabled=True,
             l10n_platforms=None,
             enUS_platforms=["win32", "macosx64"],
@@ -28,5 +45,8 @@ class TestMakeTaskGraph(unittest.TestCase):
             product="firefox",
             revision="abcdef123456",
             balrog_api_root="https://fake.balrog/api",
+            signing_class="release-signing",
         )
-        self.assertEquals(expected, generated)
+
+        self._do_common_assertions(graph)
+        self.assertEquals(graph, {})
