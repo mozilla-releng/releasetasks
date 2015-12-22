2c76c9bbbbb2f8459b1875f023643ef466e2a6d5
Justin Wood <Callek@gmail.com>
2015-12-22 16:43:54 -0500

    Add tests

--- releasetasks/test/__init__.py
+++ releasetasks/test/__init__.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+import os
+
+
+def read_file(path):
+    with open(path) as f:
+        return f.read()
+
+
+PVT_KEY = read_file(os.path.join(os.path.dirname(__file__), "id_rsa"))
+PUB_KEY = read_file(os.path.join(os.path.dirname(__file__), "id_rsa.pub"))
+OTHER_PUB_KEY = read_file(os.path.join(os.path.dirname(__file__),
+                                       "other_rsa.pub"))
