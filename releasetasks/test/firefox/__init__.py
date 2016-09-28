63cae1d6003c6fa44fe130baed711e341f19ba84
Connor Sheehan <csheehan@mozilla.com>
2016-09-28 01:48:41 -0400

    Convert common_assertions to voluptuous test. First draft of verify() function.

--- releasetasks/test/firefox/__init__.py
+++ releasetasks/test/firefox/__init__.py
@@ -7,61 +7,71 @@ import yaml
 
 from releasetasks import make_task_graph as make_task_graph_orig
 from releasetasks.test import PUB_KEY, DUMMY_PUBLIC_KEY
-from voluptuous.schema_builder import Optional, Required, Schema
-from voluptuous.validators import Any
+from voluptuous import All, Any, Optional, Required, Schema, truth
+from voluptuous.humanize import validate_with_humanized_errors
 
 
-TASK_SCHEMA = Schema({
-    Required('reruns'): Any(),
-    Required('task'): {
-        Required('priority'): 'high',
-        Required('metadata'): {
-            Required('name'): str,
-        },
-        Required('extra'): {
-            Required('task_name'): str,
-            Required('build_props'): {
-                Required('product'): str,
-                Required('locales'): str,
-                Required('branch'): str,
-                Required('platform'): str,
-                Required('version'): str,
-                Required('revision'): str,
-                Required('build_number'): str,
+@truth
+def passes_task_provisionerId_test(task):
+    if task['provisionerId'] == "buildbot-bridge":
+        assert not {"treeherderEnv", "treeherderEnv"}.intersection(task["extra"])
+        assert not any([r.startswith("tc-treeherder") for r in task["routes"]])
+
+    if task['provisionerId'] == "aws-provisioner-v1":
+        assert {"treeherder", "treeherderEnv"}.intersection(task["extra"])
+        assert len([r for r in task["routes"] if r.startswith("tc-treeherder")]) == 2
+    return True  # if this line is reached the schema is validated
+
+
+@truth
+def passes_task_signature_test(task):
+    assert task['task']['taskId'] == jwt.decode(task['task']["extra"]["signing"]["signature"], PUB_KEY, algorithms=[ALGORITHMS.RS512])['taskId']
+    return True
+
+
+@truth
+def passes_index_route_requirement(task_routes):
+    rel_routes = [r.startswith("index.releases.") for r in task_routes]
+    assert len(rel_routes) >= 2, "At least 2 release index routes required"
+    return True
+
+
+TASK_SCHEMA = Schema(All({
+    Required('reruns'): int,
+    Required('taskId'): Any(int, str),
+    Required('task'): All(
+        Schema({
+            Required('priority'): 'high',
+            Required('metadata'): {
+                Required('name'): str,
             },
-            Required('signing'): {
-                Required('signature'): str,
-            }
-        },
-        Required('payload'): {
-            Optional('properties'): {
-                Required('version'): str,
-                Required('build_number'): str,
-                Required('release_promotion'): str,
-                Required('revision'): str,
-                Required('product'): str,
-            }
-        },
-        Required('routes'): [str],
-    }
-}, extra=True)
-
-
-def generate_signature_schema(task):
-    return {
-        Required('taskId'): jwt.decode(task["extra"]["signing"]["signature"], PUB_KEY, algorithms=[ALGORITHMS.RS512])['taskId']
-    }
-
-PROVISIONER_ID_EXTENSIONS = {
-    'buildbot-bridge': {},
-    'aws-provisioner-v1': {
-        Required('provisionerId'): 'aws-provisioner-v1',
-        Required('extra'): {
-            Required('treeherder'): str,
-            Required('treeherderEnv'): str,
-        }
-    }
-}
+            Required('extra'): {
+                Required('task_name'): str,
+                Required('build_props'): {
+                    Required('product'): str,
+                    Required('locales'): Any([str], [None]),
+                    Required('branch'): str,
+                    Required('platform'): Any(str, None),
+                    Required('version'): str,
+                    Required('revision'): str,
+                    Required('build_number'): int,
+                },
+                Required('signing'): {
+                    Required('signature'): str,
+                }
+            },
+            Required('payload'): {
+                Optional('properties'): {
+                    Required('version'): str,
+                    Required('build_number'): Any(str, int),  # TODO: ask rail about this being string/int, or set in specific test
+                    Required('release_promotion'): bool,
+                    Required('revision'): str,
+                    Required('product'): str,
+                }
+            },
+            Required('routes'): passes_index_route_requirement,
+        }, extra=True), passes_task_provisionerId_test)
+}, extra=True), passes_task_signature_test)
 
 
 def create_firefox_test_args(non_standard_arguments):
@@ -76,82 +86,19 @@ def do_common_assertions(graph):
     if graph['tasks']:
         for t in graph['tasks']:
             assert t['taskId'] not in _cached_taskIDs
-
-            rel_routes = [r.startswith("index.releases.") for r in t['task']["routes"]]
-            assert len(rel_routes) >= 2, "At least 2 release index routes required"
-
-            if t['task']["provisionerId"] == "buildbot-bridge":
-                assert "treeherder" not in t['task']["extra"]
-                assert "treeherderEnv" not in t['task']["extra"]
-                assert not any([r.startswith("tc-treeherder") for r in t['task']["routes"]])
-
-            if t['task']["provisionerId"] == "aws-provisioner-v1":
-                assert "treeherder" in t['task']["extra"]
-                assert "treeherderEnv" in t['task']["extra"]
-                assert len([r for r in t['task']["routes"] if r.startswith("tc-treeherder")]) == 2
-
-            correct_schema = TASK_SCHEMA.extend(generate_signature_schema(t['task']))
-            correct_schema = correct_schema.extend(PROVISIONER_ID_EXTENSIONS.get(t['task']['provisionerId']))
-
-            assert t == correct_schema(t)
+            assert t == validate_with_humanized_errors(t, TASK_SCHEMA)
 
             _cached_taskIDs.add(t['taskId'])
 
 
-def do_common_assertions_2(graph):
-    _cached_taskIDs = set()
-    if graph["tasks"]:
-        for t in graph["tasks"]:
-            task = t["task"]
-            task_name = task["metadata"]["name"]
-            assert "reruns" in t
-            assert task["priority"] == "high"
-            assert "task_name" in task["extra"]
-            assert "build_props" in task["extra"], "inlcude common_extras.yml.tmpl"
-            for prop in ["product", "locales", "branch", "platform", "version",
-                         "revision", "build_number"]:
-                assert prop in task["extra"]["build_props"]
-            assert "signature" in task["extra"].get("signing", {}), \
-                "%s is not signed" % task["extra"]["task_name"]
-            claims = jwt.decode(task["extra"]["signing"]["signature"],
-                                PUB_KEY, algorithms=[ALGORITHMS.RS512])
-            assert claims["taskId"] == t["taskId"], \
-                "Task ID mismatch in %s signature" % task["extra"]["task_name"]
-            properties = task["payload"].get("properties")
-            if properties:
-                # The following properties are required by log_uploader.py
-                # and QE automation
-                required_properties = (
-                    "version", "build_number", "release_promotion",
-                    "revision", "product")
-                for prop in required_properties:
-                    assert prop in properties
-            # encryptedEnvs = task["payload"].get("encryptedEnv")
-            # if encryptedEnvs:
-            #    for encryptedEnv in encryptedEnvs:
-            #        # TODO: Check the encryptedEnv endTime >= task deadline
-            #        #    Requires a private key of the dummy pubkey we use.
-            #        #    Can use from taskcluster.utils import decryptMessage
-            assert t["taskId"] not in _cached_taskIDs
-            assert "routes" in task
-
-            if task["provisionerId"] == "buildbot-bridge":
-                assert "treeherder" not in task["extra"], \
-                    "remove treeherder from {}: {}".format(task_name, task)
-                assert "treeherderEnv" not in task["extra"], \
-                    "remove treeherderEnv from {}: {}".format(task_name, task)
-                assert not any([r.startswith("tc-treeherder") for r in task["routes"]]), \
-                    "remove treeherder routes from {}: {}".format(task_name, task)
-
-            if task["provisionerId"] == "aws-provisioner-v1":
-                assert "treeherder" in task["extra"], \
-                    "add treeherder to {}: {}".format(task_name, task)
-                assert "treeherderEnv" in task["extra"], \
-                    "add treeherderEnv to {}: {}".format(task_name, task)
-                assert len([r for r in task["routes"] if r.startswith("tc-treeherder")]) == 2, \
-                    "{} has to have 2 treeherder routes: {}".format(task_name, task)
-
-            _cached_taskIDs.add(t["taskId"])
+def verify(graph, schema):
+    _cached_task_ids = set()
+    if graph['tasks']:
+        test_schema = TASK_SCHEMA.update(schema)
+        for t in graph['tasks']:
+            assert t == validate_with_humanized_errors(test_schema(t))
+            assert t['taskId'] not in _cached_task_ids
+            _cached_task_ids.add(t['taskId'])
 
 
 def get_task_by_name(graph, name):
