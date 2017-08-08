1aefa5913f6468cb789ba1429aada60fbffdab8e
Rok Garbas <rok@garbas.si>
2017-08-08 20:55:11 +0200

    Bug 1386319 and Bug 1379170 (#258)
    - when release_eta is available write that updates are "scheduled"
      instead of "are available"
    - when doing rc releases (we push to 2 channles, beta and release) in
      this scenario we shouldonly send email for beta channel

--- releasetasks/test/desktop/emails/test_email_final.py
+++ releasetasks/test/desktop/emails/test_email_final.py
@@ -73,3 +73,72 @@ class TestEmailFinal(unittest.TestCase):
 
     def test_common_assertions(self):
         do_common_assertions(self.graph)
+
+
+class TestEmailFinalTwoChannels(unittest.TestCase):
+    maxDiff = 30000
+    graph = None
+    task = None
+    payload = None
+
+    def setUp(self):
+        self.task_schema = Schema({
+            'task': {
+                'provisionerId': 'aws-provisioner-v1',
+                'workerType': 'gecko-decision',
+                'payload': {
+                    'command': ['/bin/echo', 'Dummy task to tell pulse-notify to send an email'],
+                },
+                'metadata': {
+                    'name': 'firefox email release-drivers beta',
+                },
+                'extra': {
+                    'notifications': {
+                        'task-completed': {
+                            'subject': 'firefox beta 42.0b2 updates are available on the beta channel now <EOM>',
+                            'message': 'firefox beta 42.0b2 updates are available on the beta channel now <EOM>',
+                            'ids': ['release-drivers'],
+                        },
+                    },
+                },
+            },
+        }, extra=True, required=True)
+
+        test_kwargs = create_firefox_test_args({
+            'push_to_candidates_enabled': True,
+            'push_to_releases_enabled': True,
+            'signing_pvt_key': PVT_KEY_FILE,
+            'release_channels': ['beta', 'release'],
+            'final_verify_channels': ['beta'],
+            'publish_to_balrog_channels': ["release-dev", "alpha"],
+            'accepted_mar_channel_id': 'firefox-mozilla-beta',
+            'signing_cert': 'dep',
+            'moz_disable_mar_cert_verification': True,
+            'en_US_config': {
+                "platforms": {
+                    "macosx64": {'signed_task_id': 'abc', 'unsigned_task_id': 'abc', "ci_system": "tc"},
+                    "win32": {'signed_task_id': 'abc', 'unsigned_task_id': 'abc', "ci_system": "tc"},
+                    "win64": {'signed_task_id': 'abc', 'unsigned_task_id': 'abc', "ci_system": "tc"},
+                    "linux": {'signed_task_id': 'abc', 'unsigned_task_id': 'abc'},
+                    "linux64": {'signed_task_id': 'abc', 'unsigned_task_id': 'abc', "ci_system": "tc"},
+                }
+            },
+        })
+
+        self.graph = make_task_graph(**test_kwargs)
+        self.task = get_task_by_name(self.graph, "release-foo-firefox_email_beta")
+
+    def generate_task_requires_validator(self):
+        requires_sorted = sorted([get_task_by_name(self.graph, "release-foo-firefox_publish_balrog")["taskId"]])
+
+        @truth
+        def validate_task_requires(task):
+            return sorted(task['requires']) == requires_sorted
+
+        return validate_task_requires
+
+    def test_email_final(self):
+        verify(self.task, self.task_schema, self.generate_task_requires_validator())
+
+    def test_common_assertions(self):
+        do_common_assertions(self.graph)
